b510a0002504ca50c4802e2273e77bd937e1799c
Filipe Silva <filipesilva1505@gmail.com>
2018-09-28 22:42:29 -0300

    ajuste dash

--- front/material-dashboard-angular2-master/src/app/layouts/admin-layout/admin-layout.module.ts
+++ front/material-dashboard-angular2-master/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -6,9 +6,7 @@ import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
 import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 import { UpgradeComponent } from '../../upgrade/upgrade.component';
 
@@ -32,9 +30,7 @@ import {
     DashboardComponent,
     UserProfileComponent,
     TableListComponent,
-    TypographyComponent,
     IconsComponent,
-    MapsComponent,
     NotificationsComponent,
     UpgradeComponent,
   ]
