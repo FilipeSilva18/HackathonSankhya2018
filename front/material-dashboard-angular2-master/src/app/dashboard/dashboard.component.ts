9ce2c4cc2a3497b066a6b8ded9acdf416baead14
Vinicius Soares <soares7vinicius@gmail.com>
2018-09-29 15:24:00 -0300

    ajuste graficos

--- front/material-dashboard-angular2-master/src/app/dashboard/dashboard.component.ts
+++ front/material-dashboard-angular2-master/src/app/dashboard/dashboard.component.ts
@@ -176,7 +176,7 @@ export class DashboardComponent implements OnInit {
 
 
 const dataDailySalesChart: any = {
-  labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+  labels: ['Tecn.', 'Call C.', 'Sup.', 'Gestão', 'Adm.', 'Soft.', 'Op.'],
   series: [
     [12, 17, 7, 17, 23, 18, 38]
   ]
@@ -186,57 +186,37 @@ const optionsDailySalesChart: any = {
   lineSmooth: Chartist.Interpolation.cardinal({
     tension: 0
   }),
+  axisX: {
+    showGrid: false
+  },
   low: 0,
   high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
   chartPadding: { top: 0, right: 0, bottom: 0, left: 0 },
 }
 
-var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+var dailySalesChart = new Chartist.Bar('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
 
 this.startAnimationForLineChart(dailySalesChart);
 
 
 /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
 
-const dataCompletedTasksChart: any = {
-  labels: ['12p', '3p', '6p', '9p', '12p', '3a', '6a', '9a'],
-  series: [
-    [230, 750, 450, 300, 280, 240, 200, 190]
-  ]
-};
-
-const optionsCompletedTasksChart: any = {
-  lineSmooth: Chartist.Interpolation.cardinal({
-    tension: 0
-  }),
-  low: 0,
-  high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-  chartPadding: { top: 0, right: 0, bottom: 0, left: 0 }
-}
-
-var completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
-
-// start animation for the Completed Tasks Chart - Line Chart
-this.startAnimationForLineChart(completedTasksChart);
-
-
 
 /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
 
 var datawebsiteViewsChart = {
-  labels: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
+  labels: ['Seg.', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sáb.', 'Dom.'],
   series: [
-    [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
-
+    [10, 7, 3, 5, 11, 2, 3]
   ]
 };
 var optionswebsiteViewsChart = {
   axisX: {
-    showGrid: false
+    showGrid: true
   },
   low: 0,
-  high: 1000,
-  chartPadding: { top: 0, right: 5, bottom: 0, left: 0 }
+  high: 20,
+  chartPadding: { top: 0, right: 0, bottom: 0, left: 0 }
 };
 var responsiveOptions: any[] = [
   ['screen and (max-width: 640px)', {
@@ -248,7 +228,7 @@ var responsiveOptions: any[] = [
     }
   }]
 ];
-var websiteViewsChart = new Chartist.Bar('#websiteViewsChart', datawebsiteViewsChart, optionswebsiteViewsChart, responsiveOptions);
+var websiteViewsChart = new Chartist.Line('#websiteViewsChart', datawebsiteViewsChart, optionswebsiteViewsChart, responsiveOptions);
 
 //start animation for the Emails Subscription Chart
 this.startAnimationForBarChart(websiteViewsChart);
