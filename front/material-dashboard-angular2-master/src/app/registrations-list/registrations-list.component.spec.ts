0662a9b762f6ec4465edf9b97217f10498c23119
Vinicius Soares <soares7vinicius@gmail.com>
2018-09-29 04:40:58 -0300

    renomeacao componente de tabela

--- front/material-dashboard-angular2-master/src/app/registrations-list/registrations-list.component.spec.ts
+++ front/material-dashboard-angular2-master/src/app/registrations-list/registrations-list.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TableListComponent } from './table-list.component';
+import { RegistrationsListComponent } from './registrations-list.component';
 
-describe('TableListComponent', () => {
-  let component: TableListComponent;
-  let fixture: ComponentFixture<TableListComponent>;
+describe('RegistrationsListComponent', () => {
+  let component: RegistrationsListComponent;
+  let fixture: ComponentFixture<RegistrationsListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TableListComponent ]
+      declarations: [ RegistrationsListComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TableListComponent);
+    fixture = TestBed.createComponent(RegistrationsListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
